76ac99ede8cbc9fc37114d7bb9ff78694dd41b8d
laindomJS <adrijar21@gmail.com>
2022-09-18 16:11:33 -0400

    added filter - improved search

--- src/lib/main/MainContainer.svelte
+++ src/lib/main/MainContainer.svelte
@@ -5,27 +5,30 @@
     let characters = [];
     let page = 1;
     $: url = `https://rickandmortyapi.com/api/character/?page=${page}`;
-    
+    let char = "";
+
     $: {
         getCharacters(url).then(data => {
             characters = data;
         });
     }
 
-    function nextPage() {
-        page++;
+    const handleInput = (e) => char = e.target.value;
+    $: {
+        if(char.length > 2) {
+            characters = characters.filter((character) => character.name.toLowerCase().includes(char));
+        } 
     }
+    
 
-    function previousPage() {
-        page--;
-    }
 </script>
 
 
 <main class="w-full p-12">
+    <input class="p-4 m-8 w-2/5 bg-gray-400 h-12 placeholder:text-black placeholder:font-bold" type="text" value={char} on:input={handleInput} placeholder="Search character">
     <section class="w-full my-2 px-8 flex items-center justify-center space-x-10">
-        <button class="buttons" on:click={previousPage} disabled={page === 1}>Previous</button>
-        <button class="buttons" on:click={nextPage} disabled={page === 42}>Next</button>
+        <button class="buttons" on:click={() => page--} disabled={page === 1}>Previous</button>
+        <button class="buttons" on:click={() => page++} disabled={page === 42}>Next</button>
     </section>
     <main class="w-full p-12 grid grid-cols-2 gap-10 justify-items-center">
         {#each characters as character}
