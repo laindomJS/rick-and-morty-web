2dbb9ae4072535cb392d942b1f00dfcf56d20218
laindomJS <adrijar21@gmail.com>
2022-09-18 16:21:15 -0400

    controlled input - improve search

--- src/lib/main/MainContainer.svelte
+++ src/lib/main/MainContainer.svelte
@@ -8,25 +8,25 @@
     let char = "";
 
     $: {
-        getCharacters(url).then(data => {
-            characters = data;
-        });
+        getCharacters(url).then(data => characters = data);
     }
 
     const handleInput = (e) => char = e.target.value;
     $: {
         if(char.length > 2) {
             characters = characters.filter((character) => character.name.toLowerCase().includes(char));
-        } 
+        } else if (char.length === 0) {
+            getCharacters(url).then(data => characters = data);
+        }
     }
 </script>
 
 
 <section class="w-full p-12">
     
-    <input class="p-4 m-8 w-2/5 bg-gray-400 h-12 placeholder:text-black placeholder:font-bold" type="text" value={char} on:input={handleInput} placeholder="Search character">
+    <input class="p-4 m-2 w-2/5 bg-gray-400 h-12 placeholder:text-black placeholder:font-bold" type="text" value={char} on:input={handleInput} placeholder="Search character">
 
-    <section class="w-full my-2 px-8 flex items-center justify-center space-x-10">
+    <section class="w-full my-12 flex items-center justify-center space-x-10">
         <button class="buttons" on:click={() => page--} disabled={page === 1}>Previous</button>
         <button class="buttons" on:click={() => page++} disabled={page === 42}>Next</button>
     </section>
