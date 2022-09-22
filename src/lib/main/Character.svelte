ecf326618eaa9ada66c722e4be825672a00c7501
laindomJS <adrijar21@gmail.com>
2022-09-21 23:23:47 -0400

    improved styles

--- src/lib/main/Character.svelte
+++ src/lib/main/Character.svelte
@@ -3,22 +3,22 @@
 </script>
 
 
-    <article class="p-6 h-fit bg-sky-300 dark:bg-violet-700 text-white rounded-lg w-80">
+    <article class="character__card">
         <img class="object-cover rounded-lg" src={character.image} alt={character.name}>
         <div class="py-4">
-            <h2 class="font-bold text-3xl">{character.name}</h2>
+            <h2 class="text-black dark:text-white font-bold text-3xl">{character.name}</h2>
             <div class="my-4">
                 <div>
-                    <span class="font-bold text-xl">Gender:</span>
+                    <span class="font-bold dark:text-white text-black text-xl">Gender:</span>
                     <span class={character.gender === 'Male' ? 'male' : 'female'}>{character.gender}</span>
                 </div>
                 <div>
-                    <span class="font-bold text-xl">Status:</span>
+                    <span class="text-black dark:text-white font-bold text-xl">Status:</span>
                     <span class={character.status === 'Alive' ? 'alive' : 'dead'}>{character.status}</span>
                 </div>
                 <div>
-                    <span class="font-bold text-xl">Species:</span>
-                    <span class="text-xl">{character.species}</span>
+                    <span class="text-black dark:text-white font-bold text-xl">Species:</span>
+                    <span class="text-xl text-black dark:text-white">{character.species}</span>
                 </div>
             </div>
         </div>
